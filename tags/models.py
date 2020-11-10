81530efc9d47d67ff7893a710d6a016a713a9af1
mr-kaa <mr.shved@gmail.com>
2020-11-11 00:09:31 +0300

    add automatic slug generation

--- tags/models.py
+++ tags/models.py
@@ -1,4 +1,7 @@
+from extra.functions import make_unique_slug
 from django.db import models
+
+from slugify import slugify
 
 # Create your models here.
 
@@ -12,8 +15,19 @@ class Tag(models.Model):
     )
     slug = models.SlugField(
         default='_',
-        max_length=32,
+        max_length=64,
         verbose_name='Tag slug',
         unique=True,
         db_index=True
     )
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.id:
+            self.slug = make_unique_slug(Tag, slugify(self.name)[:60])
+        super().save(*args, **kwargs)
